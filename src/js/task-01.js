1b46e4b825efef103db368218719fd746d316046
Oleksii <Alex13867777@gmail.com>
2021-07-21 00:16:35 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,6 @@
+const categories = document.querySelector('#categories');
+// console.log(categories.children);
+const catArr = [...categories.children];
+console.log(`У списку ${catArr.length} категорії.`);
+
+console.log(catArr);
