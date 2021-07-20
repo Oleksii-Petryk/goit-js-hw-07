3a624c76a06a773c63c80d639634414760f66a71
Oleksii <Alex13867777@gmail.com>
2021-07-21 01:06:31 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,10 @@
-const categories = document.querySelector('#categories');
+const categoriesRef = document.querySelector('#categories');
 // console.log(categories.children);
-const catArr = [...categories.children];
+const catArr = [...categoriesRef.children];
 console.log(`У списку ${catArr.length} категорії.`);
 
 console.log(catArr);
+
+const titleFinder = catArr.forEach(title => {
+    console.log(title.children);
+});
